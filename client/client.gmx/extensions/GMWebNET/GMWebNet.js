5162930febc397178b98345cb08ec8506027a486
TeunK <teunk1@gmail.com>
2015-10-09 00:08:25 +0100

    Added client-side connection init code to contact the server by setting up sockets and awaiting acknowledgement. The acknowledgement gets stored inside messages

--- client/client.gmx/extensions/GMWebNET/GMWebNet.js
+++ client/client.gmx/extensions/GMWebNET/GMWebNet.js
@@ -2,3 +2,42 @@
  * Created by Teun on 08/10/2015.
  */
 
+
+
+var sockets     = []; //init connected sockets
+var buffer      = []; //init memory buffer
+var messages    = []; //init messages for storage
+
+
+//setup connection to server
+function connect(server_ip, server_port) {
+    var server_addr = server_ip + ":" + server_port;
+    var socket = socket_connect(server_addr);
+    syn_server(socket, "message", connection_ack);
+    return socket;
+}
+
+//get connection acknowledgement from server
+function connection_ack(data) {
+    messages.push(data);
+}
+
+
+
+/* SOCKET.IO AUXILLARY FUNCTIONALITY */
+
+// create new socket for connection between server_address (host) and return its reference
+function socket_connect(host_addr) {
+    var socket = io.connect(host_addr);
+    sockets.push(socket);
+
+    return socket
+}
+
+//send event request to server through socket
+function syn_server(socket, event, callback) {
+    if (socket)
+    {
+        socket.on(event, callback);
+    }
+}
