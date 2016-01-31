2f61c71b5d05f9d84829bef4aa4c3e4aa7359427
TeunK <teunk1@gmail.com>
2016-01-31 16:40:49 +0000

    Server optimisation for simplicity Server added update_user case for broadcasting user details

--- server/server.js
+++ server/server.js
@@ -6,17 +6,6 @@ var io          = require('socket.io').listen(server);
 
 server.listen(GLOBAL.port);
 
-function feedback_page(req,res) {
-    //Write index page response to confirm server is executing
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(
-        "<h1>"+GLOBAL.project_name+"</h1>" +
-        "<br/>" +
-        "Success! The server is currently running at "+GLOBAL.host+":"+GLOBAL.port
-    );
-    res.end();
-}
-
 
 
 /* Network Logic Handling */
@@ -33,30 +22,26 @@ io.sockets.on('connection', function(socket) {
     console.log("Incoming Connection Request..");
     requestsHandled++;
 
-    // assign id to client
-    var client_id = controller.clients.length;
     var socket_id = socket;
     var client_data = {
-        client_id : client_id,
+        client_id : controller.clients.length,
         socket_id : socket_id
     };
     controller.clients.push(client_data);
 
     // reply to client with it's new client_id
-    clearbuffer();
-    writebyte(MESSAGES['S2C'].confirm_client_id);
-    writebyte(client_id);
-    sendmessage(socket_id);
-    console.log("Sent ACK to client, assigned-ID: "+client_id);
+    initial_reply(client_data);
+
+    console.log("Sent ACK to client, assigned-ID: "+client_data.client_id);
 
     // handle incoming messages from clients
-    socket_id.on('message', function(data) {
+    client_data.socket_id.on('message', function(data) {
         requestsHandled++;
 
         // store data into buffer
         buffer      = data;
         //read the message type
-        messageid   = readbyte();
+        messageid   = bufferread();
 
         var msg_type;
         console.log("DETAILS: "+controller);
@@ -67,14 +52,14 @@ io.sockets.on('connection', function(socket) {
                 /* RECEIVE CLIENT obj_id AND USERNAME -> FORWARD DATA TO ALL CLIENTS ON NETWORK */
 
                 // Client sends it's initial details, store these
-                obj_client_name = readstring();
+                obj_client_name = bufferread();
 
                 // update all clients with new client's arrival
                 msg_type = MESSAGES['S2C'].send_client_name;
                 clearbuffer();
-                writebyte(msg_type);
-                writestring(obj_client_name);
-                send_all_clients();
+                bufferwrite(msg_type);
+                bufferwrite(obj_client_name);
+                broadcast();
                 break;
 
 
@@ -82,7 +67,7 @@ io.sockets.on('connection', function(socket) {
                 /* RECEIVE CLIENT DISCONNECT NOTIFICATION -> FORWARD LEAVING CLIENT_ID TO ALL CLIENTS */
 
                 // Receive client_id of the client that is leaving
-                var obj_client_name = readstring();
+                var obj_client_name = bufferread();
                 //remove client data from list of clients
                 controller.clients.splice(client_data, 1);
 
@@ -91,41 +76,41 @@ io.sockets.on('connection', function(socket) {
                 // Warn user that client_id has left
                 msg_type = MESSAGES['S2C'].client_disconnected;
                 clearbuffer();
-                writebyte(msg_type);
-                writeshort(obj_client_name);
-                send_all_clients();
+                bufferwrite(msg_type);
+                bufferwrite(obj_client_name);
+                broadcast();
                 break;
 
             case MESSAGES['C2S'].chat_message:
                 /* RECEIVE CHAT MESSAGE -> FORWARD MESSAGE TO ALL CLIENTS */
 
                 //load message
-                var user            = readstring();
-                var chat_message    = readstring();
+                var user            = bufferread();
+                var chat_message    = bufferread();
 
                 //spread message
                 msg_type = MESSAGES['S2C'].chat_message;
                 clearbuffer();
-                writebyte(msg_type);
-                writestring(user);
-                writestring(chat_message);
-                send_all_clients();
+                bufferwrite(msg_type);
+                bufferwrite(user);
+                bufferwrite(chat_message);
+                broadcast();
                 break;
 
             case MESSAGES['C2S'].ping:
                 /* RECEIVE CHAT MESSAGE -> FORWARD MESSAGE TO ALL CLIENTS */
 
                 // Client sends it's initial details
-                var obj_client_name = readstring();
+                var obj_client_name = bufferread();
                 //load message
-                var ping_sendtime   = readstring();
+                var ping_sendtime   = bufferread();
 
                 //return message
                 msg_type = MESSAGES['S2C'].ping;
                 clearbuffer();
-                writebyte(msg_type);
-                writestring(obj_client_name);
-                writestring(ping_sendtime);
+                bufferwrite(msg_type);
+                bufferwrite(obj_client_name);
+                bufferwrite(ping_sendtime);
                 sendmessage(socket_id);
                 break;
 
@@ -134,9 +119,9 @@ io.sockets.on('connection', function(socket) {
                 msg_type = MESSAGES['S2C'].client_count;
 
                 clearbuffer();
-                writebyte(msg_type);
-                writebyte("any");
-                writebyte(controller.clients.length);
+                bufferwrite(msg_type);
+                bufferwrite("any");
+                bufferwrite(controller.clients.length);
                 sendmessage(socket_id);
                 break;
 
@@ -147,14 +132,14 @@ io.sockets.on('connection', function(socket) {
 
                 msg_type = MESSAGES['S2C'].request_count;
                 clearbuffer();
-                writebyte(msg_type);
-                writebyte("any");
-                writebyte(avg_requests);
+                bufferwrite(msg_type);
+                bufferwrite("any");
+                bufferwrite(avg_requests);
                 sendmessage(socket_id);
                 break;
 
             case MESSAGES['C2S'].new_virtual_client:
-                var obj_client_name = readstring();
+                var obj_client_name = bufferread();
 
                 //add client name to array if not yet registered
                 if (controller.clients.indexOf(obj_client_name) == -1) {
@@ -164,7 +149,7 @@ io.sockets.on('connection', function(socket) {
                 break;
 
             case MESSAGES['C2S'].remove_virtual_client:
-                var obj_client_name = readstring();
+                var obj_client_name = bufferread();
 
                 //remove virtual client from array if registered
                 var index = controller.clients.indexOf(client_id+"."+obj_client_name);
@@ -173,20 +158,48 @@ io.sockets.on('connection', function(socket) {
                 }
 
                 break;
+
+            case MESSAGES['C2S'].update_user:
+                var client_id       = bufferread();
+                var client_name     = bufferread();
+                var client_spriteID = bufferread();
+                var client_x        = bufferread();
+                var client_y        = bufferread();
+                var client_hp       = bufferread();
+                var client_str      = bufferread();
+
+                msg_type = MESSAGES['S2C'].update_user;
+                clearbuffer();
+                bufferwrite(msg_type);
+                bufferwrite(client_id);
+                bufferwrite(client_name);
+                bufferwrite(client_spriteID);
+                bufferwrite(client_x);
+                bufferwrite(client_y);
+                bufferwrite(client_hp);
+                bufferwrite(client_str);
+                broadcast();
+
+                break;
         }
     });
 });
 
-function send_all_clients() {
+function initial_reply(client_data) {
+    clearbuffer();
+    bufferwrite(MESSAGES['S2C'].confirm_client_id);
+    bufferwrite(client_data.client_id);
+    sendmessage(client_data.socket_id);
+}
+function broadcast() {
     // send buffer-contents to ALL clients that are connected to the server
     for (var client in controller.clients) {
         if (client) {
             sendmessage(controller.clients[client].socket_id);
         }
     }
+    clearbuffer();
 }
-
-
 
 
 /* SOCKET AND BUFFER FUNCTIONS */
@@ -198,26 +211,10 @@ var buffer = [];
 function clearbuffer() {
     buffer = [];
 }
-
-//buffer writes
-function writeshort(value) {
+function bufferwrite(value) {
     buffer.push(value);
 }
-function writebyte(value) {
-    buffer.push(value);
-}
-function writestring(value) {
-    buffer.push(value);
-}
-
-//buffer reads
-function readshort() {
-    return buffer.shift();
-}
-function readstring() {
-    return buffer.shift();
-}
-function readbyte() {
+function bufferread(value) {
     return buffer.shift();
 }
 
@@ -234,5 +231,19 @@ function sendmessage(socket) {
 }
 
 
+
+
+
 // server startup message in terminal window
 console.log("Server started at "+GLOBAL.host+":"+GLOBAL.port);
+function feedback_page(req,res)
+{
+    //Write index page response to confirm server is executing
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(
+        "<h1>"+GLOBAL.project_name+"</h1>" +
+        "<br/>" +
+        "Success! The server is currently running at "+GLOBAL.host+":"+GLOBAL.port
+    );
+    res.end();
+}
